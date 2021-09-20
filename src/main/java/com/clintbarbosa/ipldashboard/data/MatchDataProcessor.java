ef0434e86663ef215f11590a819e7a3380b1592f
barbosaclint <barbosaclint@gmail.com>
2021-09-20 13:56:50 +0800

    progress yt 02 ipl series

---
+++ src/main/java/com/clintbarbosa/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,27 @@
+package com.clintbarbosa.ipldashboard.data;
+
+import com.clintbarbosa.ipldashboard.model.Match;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.item.ItemProcessor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+    private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+
+    @Override
+    public Match process(MatchInput matchInput) throws Exception {
+
+        Match match = new Match();
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(matchInput.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+        match.setVenue(matchInput.getVenue());
+
+        return null;
+    }
+}
